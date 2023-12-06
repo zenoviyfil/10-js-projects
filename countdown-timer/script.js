01099fd57c260cc9ddbcd6fb4e19f4cb2ce62682
Zenoviy Fil <fil.zenoviy@gmail.com>
2023-12-06 14:34:50 +0200

    first project

---
+++ countdown-timer/script.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById("days");
+const hoursEL = document.getElementById("hours");
+const minutesEL = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const newYear = "1 Jan 2024";
+
+function countdown() {
+    const newYearDate = new Date (newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEL.innerHTML = formatTime(hours);
+    minutesEL.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
